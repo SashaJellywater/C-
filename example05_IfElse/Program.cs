1769dc7e6b1bd8d190819afd7ad8c7a857ab954c
Aleksandra <sasha-skaliarka@mail.ru>
2023-03-27 22:53:47 +0300

    example05_IfElse

---
+++ example05_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите имя пользователя");
+string username = Console.ReadLine();
+if(username.ToLower() == "luna")
+{
+    Console.WriteLine("Hi, little kitty!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
