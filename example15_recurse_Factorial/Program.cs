85fe63ccb10743c35aaaca3a745f920cb20b79df
Aleksandra <sasha-skaliarka@mail.ru>
2023-04-15 00:01:31 +0300

    Lesson 4

---
+++ example15_recurse_Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// int Factorial(int n)
+// {
+//     if (n==1) return 1;
+//     else return n*Factorial(n-1);
+// }
+// // переполненние типа при запуске:
+// for(int i =1;i<40;i++)
+// {
+// Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+double Factorial(int n) //меняем int на double
+{
+    if (n==1) return 1;
+    else return n*Factorial(n-1);
+}
+// переполненние типа при запуске:
+for(int i =1;i<40;i++)
+{
+Console.WriteLine($"{i}! = {Factorial(i)}");
+}
